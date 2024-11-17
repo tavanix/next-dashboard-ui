74887ad7f6a0e9b4c61163e39a586ea7ae86ee41
Maksim Aisin <maksim.aisin@mail.ru>
2024-11-17 15:55:36 +0300

    feat: layout, menu, navbar

---
+++ src/app/(dashboard)/parent/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ParentPage() {
+  return <div>parent</div>
+}
+
+export default ParentPage
---
+++ src/app/(dashboard)/student/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function StudentPage() {
+  return <div></div>
+}
+
+export default StudentPage
