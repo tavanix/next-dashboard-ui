fbe84aa5f042c34e172dcdf4d7e5371ab0632b95
Maksim Aisin <maksim.aisin@mail.ru>
2024-11-24 16:55:16 +0300

    feat: announcements

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,52 @@
+function Announcements() {
+  return (
+    <div className='bg-white p-4 rounded-md'>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-xl font-semibold'>Announcements</h1>
+        <span className='text-sm text-gray-400'>View all</span>
+      </div>
+      <div className='flex flex-col gap-4 mt-4'>
+        <div className='bg-mySkyLight rounded-md p-4'>
+          <div className='flex items-center justify-between'>
+            <h2 className='font-medium'>Lorem ipsum dolor sit.</h2>
+            <span className='text-sm text-gray-400 bg-white rounded-md px-1 py-1'>
+              2024-01-01
+            </span>
+          </div>
+          <p className='text-sm text-gray-400 mt-1'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim,
+            facere.
+          </p>
+        </div>
+
+        <div className='bg-myPurpleLight rounded-md p-4'>
+          <div className='flex items-center justify-between'>
+            <h2 className='font-medium'>Lorem ipsum dolor sit.</h2>
+            <span className='text-sm text-gray-400 bg-white rounded-md px-1 py-1'>
+              2024-01-01
+            </span>
+          </div>
+          <p className='text-sm text-gray-400 mt-1'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim,
+            facere.
+          </p>
+        </div>
+
+        <div className='bg-myYellowLight rounded-md p-4'>
+          <div className='flex items-center justify-between'>
+            <h2 className='font-medium'>Lorem ipsum dolor sit.</h2>
+            <span className='text-sm text-gray-400 bg-white rounded-md px-1 py-1'>
+              2024-01-01
+            </span>
+          </div>
+          <p className='text-sm text-gray-400 mt-1'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim,
+            facere.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Announcements
